058907050efc641a3e75c5eec16962b5d506605e
Sergey Olifir <sergey.olifir@gmail.com>
2012-03-18 00:32:14 +0200

    vorcing LIFE

--- GUI.js
+++ GUI.js
@@ -1,13 +1,26 @@
-function DrawWorldTable( Table, iRow, iCol) {
+function DrawWorldTable( Table, iRow, iCol, aWorld) {
+	Table.empty();
 	for(var y = 0, ii = 0; y < iRow; y++) {
 		var tr = document.createElement('TR');
-		for(var x = 0; x < iCol; x++, ii++){
+		for(var x = 0; x < iCol; x++, ii++) {
 			var td = document.createElement('TD');
 			td.id = y + '_' + x;
 			td.width = 16;
 			td.height = 16;
+			td.row = y;
+			td.col = x;
+			if(aWorld[y][x]) td.classList.add('item');
+			//
+			//console.log(fRandom && Math.random()>0.5);
 			tr.appendChild(td);
 		}
 		Table.append(tr);
 	}
+	$('td').click(function() {
+		//console.log('[' + this.row + ' , ' + this.col + ']');
+		//console.log(zaz);
+		zaz.revert(this.row,this.col);// = !zaz.world[this.row][this.col];
+		console.log(zaz);
+		$(this).toggleClass('item', 0);
+	});
 }
--- Game_of_Life.js
+++ Game_of_Life.js
@@ -1,3 +1,124 @@
+GameLife = function(config) {
+	this.world = new Array();
+	var Col = config.col;
+	var Row = config.row;
+	var worl_area = config.area;
+	
+	this.init = function() {
+		this.world = new Array();
+		for(var i = 0; i < Col; i++) {
+			this.world[i] = new Array();
+			for(var j = 0; j < Row; j++) {
+				this.world[i][j] = false;
+			}
+		}
+		DrawWorldTable(worl_area, Row, Col, this.world);
+	}
+	
+	this.revert = function(iRow, iCol) {
+		this.world[iRow][iCol] = !this.world[iRow][iCol];
+	}
+	
+	this.initRandom = function() {
+		this.world = new Array();
+		for(var i = 0; i < Col; i++) {
+			this.world[i] = new Array();
+			for(var j = 0; j < Row; j++) {
+				this.world[i][j] = Math.random() > 0.5;
+			}
+		}
+		DrawWorldTable(worl_area, Row, Col, this.world);
+	}
+	
+	this.interval = 0;
+	this.start = function() {
+		//interval = setInterval(this.doStep, 1000);
+		window.setTimeout(this.doStep, 1000);
+	}
+	
+	this.doStep = function() {
+		console.log(this.world);
+		var res = 0;
+		for(var i = 0; i < Row; i++) {
+			for(var j = 0; j < Col; j++) {
+				res = 0;
+				if(this.world[i-1] != undefined) {
+					if(this.world[i-1][j-1] != undefined) {
+						res += this.world[i-1][j-1];
+					}
+				}
+				if(this.world[i-1] != undefined) {
+					if(this.world[i-1][j] != undefined) {
+						res += this.world[i-1][j];
+					}
+				}
+				if(this.world[i-1] != undefined) {
+					if(this.world[i-1][j+1] != undefined) {
+						res += this.world[i-1][j+1];
+					}
+				}
+				if(this.world[i] != undefined) {
+					if(this.world[i][j+1] != undefined) {
+						res += this.world[i][j+1];
+					}
+				}
+				if(this.world[i] != undefined) {
+					if(this.world[i][j-1] != undefined) {
+						res += this.world[i][j-1];
+					}
+				}
+				if(this.world[i+1] != undefined) {
+					if(this.world[i+1][j+1] != undefined) {
+						res += this.world[i+1][j+1];
+					}
+				}
+				if(this.world[i+1] != undefined) {
+					if(this.world[i+1][j] != undefined) {
+						res += this.world[i+1][j];
+					}
+				}
+				if(this.world[i+1] != undefined) {
+					if(this.world[i+1][j-1] != undefined) {
+						res += this.world[i+1][j-1];
+					}
+				}
+				if(this.world[i][j]) {
+					if(res > 3) {
+						this.world[i][j] = false;
+					}
+					if(res < 2) {
+						this.world[i][j] = false;
+					}
+				} else {
+					if(res == 3) {
+						this.world[i][j] = true;
+					}
+				}
+			}
+		}
+		DrawWorldTable(worl_area, Row, Col, this.world);
+	}
+}
+
+
 $(document).ready(function() {
-  DrawWorldTable($('#poligon'), 10, 10);
+	$('#start').click(function(){
+		zaz = new GameLife({
+			col: $('#Row').val(), 
+			row: $('#Col').val(),
+			area: $('#poligon'),
+		});
+		zaz.init();
+		//console.log($('#Row').val());
+		//DrawWorldTable($('#poligon'), , $('#Col').val());
+	});
+	
+	$('#rand').click(function() {
+		zaz.initRandom();
+	});
+	$('#go').click(function() {
+		//zaz.start();
+		zaz.doStep();
+	});
+  //DrawWorldTable($('#poligon'), 10, 10);
 });
