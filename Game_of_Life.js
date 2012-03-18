469eebf5357ea782b59a97692f536ff4b50cb6ca
Sergey Olifir <sergey.olifir@gmail.com>
2012-03-18 02:01:37 +0200

    fix errors in algoritm

--- Game_of_Life.js
+++ Game_of_Life.js
@@ -1,103 +1,176 @@
+world = new Array();
 GameLife = function(config) {
-	this.world = new Array();
+	
 	var Col = config.col;
 	var Row = config.row;
 	var worl_area = config.area;
 	
 	this.init = function() {
-		this.world = new Array();
-		for(var i = 0; i < Col; i++) {
-			this.world[i] = new Array();
-			for(var j = 0; j < Row; j++) {
-				this.world[i][j] = false;
+		world = new Array();
+		for(var i = 0; i < Row; i++) {
+			world[i] = new Array();
+			for(var j = 0; j < Col; j++) {
+				world[i][j] = false;
 			}
 		}
-		DrawWorldTable(worl_area, Row, Col, this.world);
+		DrawWorldTable(worl_area, Row, Col, world);
 	}
 	
 	this.revert = function(iRow, iCol) {
-		this.world[iRow][iCol] = !this.world[iRow][iCol];
+		world[iRow][iCol] = !world[iRow][iCol];
 	}
 	
 	this.initRandom = function() {
-		this.world = new Array();
-		for(var i = 0; i < Col; i++) {
-			this.world[i] = new Array();
-			for(var j = 0; j < Row; j++) {
-				this.world[i][j] = Math.random() > 0.5;
+		world = new Array();
+		for(var i = 0; i < Row; i++) {
+			world[i] = new Array();
+			for(var j = 0; j < Col; j++) {
+				world[i][j] = Math.random() > 0.5;
 			}
 		}
-		DrawWorldTable(worl_area, Row, Col, this.world);
+		DrawWorldTable(worl_area, Row, Col, world);
 	}
-	
+	This = this;
 	this.interval = 0;
 	this.start = function() {
 		//interval = setInterval(this.doStep, 1000);
-		window.setTimeout(this.doStep, 1000);
+		window.setTimeout(This.doStep, 1000);
 	}
 	
 	this.doStep = function() {
-		console.log(this.world);
+		var tmp_world = new Array();
+		for(var i = 0; i < Row; i++) {
+			tmp_world[i] = new Array();
+			for(var j = 0; j < Col; j++) {
+				tmp_world[i][j] = world[i][j];
+			}
+		}
 		var res = 0;
 		for(var i = 0; i < Row; i++) {
 			for(var j = 0; j < Col; j++) {
 				res = 0;
-				if(this.world[i-1] != undefined) {
-					if(this.world[i-1][j-1] != undefined) {
-						res += this.world[i-1][j-1];
+				if(world[i-1] != undefined) {
+					if(world[i-1][j-1] != undefined) {
+						res += world[i-1][j-1];
 					}
 				}
-				if(this.world[i-1] != undefined) {
-					if(this.world[i-1][j] != undefined) {
-						res += this.world[i-1][j];
+				if(world[i-1] != undefined) {
+					if(world[i-1][j] != undefined) {
+						res += world[i-1][j];
 					}
 				}
-				if(this.world[i-1] != undefined) {
-					if(this.world[i-1][j+1] != undefined) {
-						res += this.world[i-1][j+1];
+				if(world[i-1] != undefined) {
+					if(world[i-1][j+1] != undefined) {
+						res += world[i-1][j+1];
 					}
 				}
-				if(this.world[i] != undefined) {
-					if(this.world[i][j+1] != undefined) {
-						res += this.world[i][j+1];
+				if(world[i] != undefined) {
+					if(world[i][j+1] != undefined) {
+						res += world[i][j+1];
 					}
 				}
-				if(this.world[i] != undefined) {
-					if(this.world[i][j-1] != undefined) {
-						res += this.world[i][j-1];
+				if(world[i] != undefined) {
+					if(world[i][j-1] != undefined) {
+						res += world[i][j-1];
 					}
 				}
-				if(this.world[i+1] != undefined) {
-					if(this.world[i+1][j+1] != undefined) {
-						res += this.world[i+1][j+1];
+				if(world[i+1] != undefined) {
+					if(world[i+1][j+1] != undefined) {
+						res += world[i+1][j+1];
 					}
 				}
-				if(this.world[i+1] != undefined) {
-					if(this.world[i+1][j] != undefined) {
-						res += this.world[i+1][j];
+				if(world[i+1] != undefined) {
+					if(world[i+1][j] != undefined) {
+						res += world[i+1][j];
 					}
 				}
-				if(this.world[i+1] != undefined) {
-					if(this.world[i+1][j-1] != undefined) {
-						res += this.world[i+1][j-1];
+				if(world[i+1] != undefined) {
+					if(world[i+1][j-1] != undefined) {
+						res += world[i+1][j-1];
 					}
 				}
-				if(this.world[i][j]) {
+				if(world[i][j]) {
 					if(res > 3) {
-						this.world[i][j] = false;
+						tmp_world[i][j] = false;
 					}
 					if(res < 2) {
-						this.world[i][j] = false;
+						tmp_world[i][j] = false;
 					}
 				} else {
 					if(res == 3) {
-						this.world[i][j] = true;
+						tmp_world[i][j] = true;
 					}
 				}
 			}
 		}
-		DrawWorldTable(worl_area, Row, Col, this.world);
+		world = new Array();
+		for(var i = 0; i < Row; i++) {
+			world[i] = new Array();
+			for(var j = 0; j < Col; j++) {
+				world[i][j] = tmp_world[i][j];
+			}
+		}
+		This.DrawWorldTable(worl_area, Row, Col, world);
 	}
+	
+	this.DrawWorldTable = function( Table, iRow, iCol, aWorld) {
+		Table.empty();
+		var item_count = 0;
+		for(var y = 0, ii = 0; y < iRow; y++) {
+			var tr = document.createElement('TR');
+			for(var x = 0; x < iCol; x++, ii++) {
+				var td = document.createElement('TD');
+				td.id = y + '_' + x;
+				td.width = 16;
+				td.height = 16;
+				td.row = y;
+				td.col = x;
+				if(world[y][x]) {
+					td.classList.add('item');
+					item_count++;
+				}
+				tr.appendChild(td);
+			}
+			Table.append(tr);
+		}
+		if(item_count) {
+			this.start();
+		} else {
+			alert('Все!!!');
+		}
+		
+		$('td').click(function() {
+			world[this.row][this.col] = !world[this.row][this.col];
+			$(this).toggleClass('item', 0);
+		});
+	}
+	
+	function DrawWorldTable( Table, iRow, iCol, aWorld) {
+		Table.empty();
+		var item_count = 0;
+		for(var y = 0, ii = 0; y < iRow; y++) {
+			var tr = document.createElement('TR');
+			for(var x = 0; x < iCol; x++, ii++) {
+				var td = document.createElement('TD');
+				td.id = y + '_' + x;
+				td.width = 16;
+				td.height = 16;
+				td.row = y;
+				td.col = x;
+				if(world[y][x]) {
+					td.classList.add('item');
+					item_count++;
+				}
+				tr.appendChild(td);
+			}
+			Table.append(tr);
+		}
+		
+		$('td').click(function() {
+			world[this.row][this.col] = !world[this.row][this.col];
+			$(this).toggleClass('item', 0);
+		});
+}
 }
 
 
